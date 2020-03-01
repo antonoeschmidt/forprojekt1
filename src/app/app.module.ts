ae11bfff2791cad860924534e8be670813f4d209
antonoeschmidt <s163053@student.dtu.dk>
2020-03-01 17:29:45 +0700

    Projekt oprettet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { GalgegameComponent } from './galgegame/galgegame.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    GalgegameComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/galgegame/galgegame.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-galgegame',
+  templateUrl: './galgegame.component.html',
+  styleUrls: ['./galgegame.component.css']
+})
+export class GalgegameComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
