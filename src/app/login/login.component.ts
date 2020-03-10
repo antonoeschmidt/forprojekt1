775487cb4c425e47a5276f9def8b8adbbe5acace
antonoeschmidt <s163053@student.dtu.dk>
2020-03-10 09:42:37 +0100

    Knapper og billeder lavet + navigation

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  @Output() eventEmitterLogin = new EventEmitter<boolean>();
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
 
+  clickOnLoginBtn(value: boolean) {
+    this.eventEmitterLogin.emit(value);
+  }
 }
