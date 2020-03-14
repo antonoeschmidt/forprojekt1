b30f17938c7bcb0cfd36cdd06f15c9739f7a392e
antonoeschmidt <s163053@student.dtu.dk>
2020-03-14 20:54:17 +0100

    POST til RMI brugeraut. virker

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -8,28 +8,27 @@ import {HttpClient} from '@angular/common/http';
 })
 export class LoginComponent implements OnInit {
   @Output() eventEmitterLogin = new EventEmitter<boolean>();
-  posts;
 
-  constructor() {
+  constructor(private http: HttpClient) {
   }
 
   ngOnInit() {
   }
 
-  clickOnLoginBtn(value: boolean) {
-    // this.http.get('http://localhost:8090/rest/hej').subscribe(data => {
-    //   console.log(data);
-    // });
+  clickOnLoginBtn(user: string, pass: string) {
+    if (user == '' || pass == '') {
+      alert("Indtast studienummer og kode");
+      return;
+    }
 
-
-
-    // tslint:disable-next-line:max-line-length
-    // this.http.get('https://www.dr.dk/mu-online/api/1.3/list/' +
-    //   'view/mostviewed?channel=dr1&channeltype=TV&limit=5&offset=4')
-    //   .subscribe(data => {
-    //   console.log(data);
-    // });
-
-
+    this.http.post('http://localhost:8080/login', {
+      user: user,
+      pass: pass
+    })
+      .toPromise()
+      .then((data: boolean) => {
+        console.log(data);
+        data ? this.eventEmitterLogin.emit(data) : alert("Forkert login")
+      });
   }
 }
