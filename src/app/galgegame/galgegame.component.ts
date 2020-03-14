c742b058459e3ec39859cb12d58abea65e86af27
antonoeschmidt <s163053@student.dtu.dk>
2020-03-14 21:04:27 +0100

    Fixed comments

--- src/app/galgegame/galgegame.component.ts
+++ src/app/galgegame/galgegame.component.ts
@@ -120,7 +120,6 @@ export class GalgegameComponent implements OnInit {
   }
 
   // legacy:
-
   // bedre metode, men virker ikke 100%
   // guessLetter(value: string) {
   //   this.http.post('http://localhost:8080/guess', value)
@@ -138,25 +137,4 @@ export class GalgegameComponent implements OnInit {
   //       }
   //     );
   // }
-
-
-//   this.http.get('http://localhost:8080/newgame').toPromise()
-// .then(data => {
-//   for (let key in data) {
-//   if (data.hasOwnProperty(key)) {
-//   this.dataArray.push(data[key]);
-// }
-// }
-// this.gameData = new GameModel(this.dataArray[0], this.dataArray[1],
-//   this.dataArray[2], this.dataArray[3], this.dataArray[4]);
-// console.log('Visible word: ' + this.gameData.visibleWord);
-// console.log('Lives: ' + this.gameData.lives);
-// console.log('Used letters: ' + this.gameData.usedLetters);
-// console.log('IsGameOver: ' + this.gameData.isGameOver);
-// console.log('StatusMsg : ' + this.gameData.statusMsg);
-// console.log(this.dataArray);
-// this.dataArray = [];
-// });
-
-
 }
